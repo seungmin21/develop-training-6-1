e4a41372487becf1a847ea2e12677f8deb6306dd
ohseungmin <ohmin2886@gmail.com>
2023-12-07 16:05:24 +0900

    container 함수에 있는 함수들 전체 출력

---
+++ app.js
@@ -0,0 +1,14 @@
+function container() {
+  function itemOne() {
+    return "<h1>";
+  }
+  function itemTwo() {
+    return "Hello";
+  }
+  function itemThree() {
+    return "</h1>";
+  }
+  return itemOne + itemTwo + itemThree;
+}
+
+console.log(container());
